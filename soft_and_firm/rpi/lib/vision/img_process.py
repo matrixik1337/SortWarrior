63f6e09fe1b54dd317b1fb6308efd5ad1d74b7cb
tsygaelectronics <romanoidohm@gmail.com>
2025-12-28 19:25:12 +0300

    Calibrated color ranges and upgraded the img_process.py funcs

--- soft_and_firm/rpi/lib/vision/img_process.py
+++ soft_and_firm/rpi/lib/vision/img_process.py
@@ -1,88 +1,205 @@
 import cv2
 import numpy as np
 
-#RED is 0
-#BLUE is 1
+# ALL COLORS is 0
+# RED color is 1
+# BLUE color is 2
+
+red_lower = np.array([0,50,50])
+red_upper = np.array([20,255,255])
+blue_lower = np.array([110,50,50])
+blue_upper = np.array([130,255,255])
+
+red_ui_color = (127,127,255)
+blue_ui_color = (255,127,127)
+
 
 class ImgProcess:
-    def get_pucks_count(self,frame,color,startX,startY,endX,endY):
+    def get_pucks_count(self,frame,color,startX,startY,endX,endY): # Gettings count of pucks on frame in predefined area
       result_frame = frame.copy()
       cv2.rectangle(result_frame,(int(startX),int(startY)),(int(endX), int(endY)), (50,50,255),4)
     
       count = 0
-      if color==0:
-        lower = np.array([0,150,150])
-        upper = np.array([0,255,255])
-      elif color==1:
-        lower = np.array([120,150,150])
-        upper = np.array([120,255,255])
 
-      hsv_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-      mask = cv2.inRange(hsv_frame,lower,upper)
-      contours, hierarchy = cv2.findContours(mask,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+      # Range depended on color
 
-      objData = []
+      if color == 0:
+        hsv_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
 
-      for _, c in enumerate(contours):
-        boundRect = cv2.boundingRect(c)
-        objData.append((count, boundRect))
+        objData = []
+
+        lower = red_lower
+        upper = red_upper
+        mask = cv2.inRange(hsv_frame,lower,upper)
+        contours, hierarchy = cv2.findContours(mask,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        for _, c in enumerate(contours): # Count and mark all pucks
+          boundRect = cv2.boundingRect(c)
+          objData.append((count, boundRect))
 
 
-        rectX = int(boundRect[0])
-        rectY = int(boundRect[1])
-        rectWidth = int(boundRect[2])
-        rectHeight = int(boundRect[3])
-        if rectX>startX and rectX+rectWidth<endX and rectY>startY and rectY+rectHeight<endY:
-          count += 1 
-          cv2.putText(result_frame,str("X:")+str((rectX+rectWidth)/2)+str(";")+str("Y:")+str((rectY+rectHeight)/2),(rectX,rectY-2),cv2.FONT_HERSHEY_SIMPLEX,0.4,(0,255,0),1)
-          cv2.rectangle(result_frame,(rectX, rectY), (rectX + rectWidth, rectY + rectHeight), (0,255,0), 2)
+          rectX = int(boundRect[0])
+          rectY = int(boundRect[1])
+          rectWidth = int(boundRect[2])
+          rectHeight = int(boundRect[3])
+          if rectX>startX and rectX+rectWidth<endX and rectY>startY and rectY+rectHeight<endY:
+            count += 1 
+            cv2.putText(result_frame,str("X:")+str((rectX+rectWidth)/2)+str(";")+str("Y:")+str((rectY+rectHeight)/2),(rectX,rectY-2),cv2.FONT_HERSHEY_SIMPLEX,0.4,(0,255,0),1)
+            cv2.rectangle(result_frame,(rectX, rectY), (rectX + rectWidth, rectY + rectHeight), (0,255,0), 2)
+            cv2.drawMarker(result_frame,(int(rectX+(rectWidth/2)),int(rectY+(rectHeight/2))),red_ui_color,cv2.MARKER_DIAMOND,10,3)
 
-      cv2.putText(result_frame,str("PUCKS: ")+str(count),(10,40),cv2.FONT_HERSHEY_SIMPLEX,0.75,(0,255,0),2)
+        lower = blue_lower
+        upper = blue_upper
+        mask = cv2.inRange(hsv_frame,lower,upper)
+        contours, hierarchy = cv2.findContours(mask,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        for _, c in enumerate(contours): # Count and mark all pucks
+          boundRect = cv2.boundingRect(c)
+          objData.append((count, boundRect))
+
+
+          rectX = int(boundRect[0])
+          rectY = int(boundRect[1])
+          rectWidth = int(boundRect[2])
+          rectHeight = int(boundRect[3])
+          if rectX>startX and rectX+rectWidth<endX and rectY>startY and rectY+rectHeight<endY:
+            count += 1 
+            cv2.putText(result_frame,str("X:")+str((rectX+rectWidth)/2)+str(";")+str("Y:")+str((rectY+rectHeight)/2),(rectX,rectY-2),cv2.FONT_HERSHEY_SIMPLEX,0.4,(0,255,0),1)
+            cv2.rectangle(result_frame,(rectX, rectY), (rectX + rectWidth, rectY + rectHeight),(0,255,0), 2)
+            cv2.drawMarker(result_frame,(int(rectX+(rectWidth/2)),int(rectY+(rectHeight/2))),blue_ui_color,cv2.MARKER_DIAMOND,10,3)
+
+      else:
+        if color==1:
+          lower = red_lower
+          upper = red_upper
+          ui_color = red_ui_color
+        elif color==2:
+          lower = blue_lower
+          upper = blue_upper
+          ui_color = blue_ui_color
+
+        hsv_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv_frame,lower,upper)
+        contours, hierarchy = cv2.findContours(mask,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        objData = []
+
+        for _, c in enumerate(contours): # Count and mark all pucks
+          boundRect = cv2.boundingRect(c)
+          objData.append((count, boundRect))
+
+
+          rectX = int(boundRect[0])
+          rectY = int(boundRect[1])
+          rectWidth = int(boundRect[2])
+          rectHeight = int(boundRect[3])
+          if rectX>startX and rectX+rectWidth<endX and rectY>startY and rectY+rectHeight<endY:
+            count += 1 
+            cv2.putText(result_frame,str("X:")+str((rectX+rectWidth)/2)+str(";")+str("Y:")+str((rectY+rectHeight)/2),(rectX,rectY-2),cv2.FONT_HERSHEY_SIMPLEX,0.4,(0,255,0),1)
+            cv2.rectangle(result_frame,(rectX, rectY), (rectX + rectWidth, rectY + rectHeight), (0,255,0), 2)
+            cv2.drawMarker(result_frame,(int(rectX+(rectWidth/2)),int(rectY+(rectHeight/2))),ui_color,cv2.MARKER_DIAMOND,10,3)
 
       return count, result_frame
 
-    def track_nearest_puck(self,frame,color):
+    def track_nearest_puck(self,frame,color,startX,startY,endX,endY): # Getting coordinates of nearest puck in predefined area
       result_frame = frame.copy()
-    
-      count = 0
-      if color==0:
-        lower = np.array([250,150,150])
-        upper = np.array([5,255,255])
-      elif color==1:
-        lower = np.array([115,150,150])
-        upper = np.array([125,255,255])
+      cv2.rectangle(result_frame,(int(startX),int(startY)),(int(endX), int(endY)), (50,50,255),4)
 
-      hsv_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-      mask = cv2.inRange(hsv_frame,lower,upper)
-      contours, hierarchy = cv2.findContours(mask,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-      objData = []
 
       x=0
       y=0
       width = 0
       height = 0
 
-      for _, c in enumerate(contours):
-        boundRect = cv2.boundingRect(c)
-        objData.append((count, boundRect))
+      if color==0:
+        count=0
+
+        hsv_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+
+        lower = red_lower
+        upper = red_upper
+        mask = cv2.inRange(hsv_frame,lower,upper)
+        contours, hierarchy = cv2.findContours(mask,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        objData = []
+
+        for _, c in enumerate(contours):
+          boundRect = cv2.boundingRect(c)
+          objData.append((count, boundRect))
 
 
-        rectX = int(boundRect[0])
-        rectY = int(boundRect[1])
-        rectWidth = int(boundRect[2])
-        rectHeight = int(boundRect[3])
-        if rectWidth>width:
-          x=rectX
-          y=rectY
-          width=rectWidth
-          height=rectHeight
-          
-      cv2.putText(result_frame,str("X:")+str((x+width)/2)+str(";")+str("Y:")+str((y+height)/2),(x,y-2),cv2.FONT_HERSHEY_SIMPLEX,0.4,(0,255,0),1)    
+          rectX = int(boundRect[0])
+          rectY = int(boundRect[1])
+          rectWidth = int(boundRect[2])
+          rectHeight = int(boundRect[3])
+          if rectX>startX and rectX+rectWidth<endX and rectY>startY and rectY+rectHeight<endY:
+            if rectWidth>width:
+              x=rectX
+              y=rectY
+              width=rectWidth
+              height=rectHeight
+
+
+        lower = blue_lower
+        upper = blue_upper
+        mask = cv2.inRange(hsv_frame,lower,upper)
+        contours, hierarchy = cv2.findContours(mask,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        objData = []
+
+        for _, c in enumerate(contours):
+          boundRect = cv2.boundingRect(c)
+          objData.append((count, boundRect))
+
+
+          rectX = int(boundRect[0])
+          rectY = int(boundRect[1])
+          rectWidth = int(boundRect[2])
+          rectHeight = int(boundRect[3])
+          if rectX>startX and rectX+rectWidth<endX and rectY>startY and rectY+rectHeight<endY:
+            if rectWidth>width:
+              x=rectX
+              y=rectY
+              width=rectWidth
+              height=rectHeight
+
+      else:
+
+        count=0
+        if color==1:
+          lower = red_lower
+          upper = red_upper
+        elif color==2:
+          lower = blue_lower
+          upper = blue_upper
+
+        hsv_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv_frame,lower,upper)
+        contours, hierarchy = cv2.findContours(mask,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        objData = []
+
+        x=0
+        y=0
+        width = 0
+        height = 0
+
+        for _, c in enumerate(contours):
+          boundRect = cv2.boundingRect(c)
+          objData.append((count, boundRect))
+
+
+          rectX = int(boundRect[0])
+          rectY = int(boundRect[1])
+          rectWidth = int(boundRect[2])
+          rectHeight = int(boundRect[3])
+          if rectX>startX and rectX+rectWidth<endX and rectY>startY and rectY+rectHeight<endY:
+            if rectWidth>width:
+              x=rectX
+              y=rectY
+              width=rectWidth
+              height=rectHeight
+            
+      cv2.putText(result_frame,str("X:")+str(x+(width/2))+str(";")+str("Y:")+str(y+(height/2)),(x,y-2),cv2.FONT_HERSHEY_SIMPLEX,0.4,(0,255,0),1)    
       cv2.rectangle(result_frame,(x, y), (x + width, y + height), (0,255,0), 2)
 
-      cv2.drawMarker(result_frame,(int((x+width)/2),int((y+height)/2)),(255,255,255),cv2.MARKER_CROSS,1,1)
+      cv2.drawMarker(result_frame,(int(x+(width/2)),int(y+(height/2))),(255,255,255),cv2.MARKER_TILTED_CROSS,10,5)
 
-      return (x+width)/2, (y+height)/2, result_frame
-    
-    def track_pucks(self,frame,)
+      return x+(width/2), y+(height/2), result_frame
+  
--- soft_and_firm/rpi/test.py
+++ soft_and_firm/rpi/test.py
@@ -1,26 +1,41 @@
-from lib.vision.camera import Camera
 from lib.vision.img_process import ImgProcess
+from time import sleep
 import cv2
 
-print("Initialize camera... ", end="")
-try:
-    cam = Camera(3)
-    print("Success!")
-except Exception as e:
-    print(f"ERROR: {e}!")
+improc = ImgProcess()
 
-print("Initialize OpenCV... ", end="")
-try:
-    improc = ImgProcess()
-    print("Success!")
-except Exception as e:
-    print(f"ERROR: {e}!")
+frame = cv2.imread("images_for_test/img1.png")
 
-while(1):
-    x, y,frame = improc.track_nearest_puck(cam.get_array(),1)
-    cv2.imshow("Frame",frame)
-    if(cv2.waitKey(1)==ord("q")):
-        break
+
+pucks_count, result_frame = improc.get_pucks_count(frame,0,10,10,920,650)
+cv2.imshow("result_frame",result_frame)
+print(f"Pucks count: {pucks_count}")
+cv2.waitKey(1000)
+
+red_pucks_count, result_frame = improc.get_pucks_count(frame,1,10,10,920,650)
+cv2.imshow("result_frame",result_frame)
+print(f"Red pucks count: {red_pucks_count}")
+cv2.waitKey(1000)
+
+blue_pucks_count, result_frame = improc.get_pucks_count(frame,2,10,10,920,650)
+cv2.imshow("result_frame",result_frame)
+print(f"Blue pucks count: {blue_pucks_count}")
+cv2.waitKey(1000)
+
+x,y, result_frame = improc.track_nearest_puck(frame,0,10,10,920,650)
+cv2.imshow("result_frame",result_frame)
+print(f"Coordinates of nearest puck: x={x};y={y}")
+cv2.waitKey(1000)
+
+red_x,red_y, result_frame = improc.track_nearest_puck(frame,1,10,10,920,650)
+cv2.imshow("result_frame",result_frame)
+print(f"Coordinates of nearest puck: x={red_x};y={red_y}")
+cv2.waitKey(1000)
+
+blue_x,blue_y, result_frame = improc.track_nearest_puck(frame,2,10,10,920,650)
+cv2.imshow("result_frame",result_frame)
+print(f"Coordinates of nearest puck: x={blue_x};y={blue_y}")
+cv2.waitKey(1000)
+
 
 cv2.destroyAllWindows()
-cam.stop()
