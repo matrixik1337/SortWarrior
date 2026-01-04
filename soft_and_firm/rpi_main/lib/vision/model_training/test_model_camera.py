12ac3b0d45fe269e246ef9983028b96432d9d44c
tsygaelectronics <romanoidohm@gmail.com>
2026-01-04 21:24:06 +0300

    added model testing with camera script

---
+++ soft_and_firm/rpi_main/lib/vision/model_training/test_model_camera.py
@@ -0,0 +1,39 @@
+from ..camera import Camera
+from ultralytics import YOLO
+import cv2
+
+path_to_model = input("Enter path to model: ")
+
+cam = Camera(3)
+
+model = YOLO(path_to_model,task="detect",verbose=False)
+model.cpu()
+
+cv2.namedWindow("result",cv2.WINDOW_NORMAL)
+
+while cv2.waitKey(1)!=ord("q"):
+    frame = cam.get_array()
+
+    detections = model.track()
+    result_frame = frame.copy()
+    results = model.track(frame, stream=True)
+
+    for result in results:
+        class_names = result.names
+        for box in result.boxes:
+            if box.conf[0] > 0.4:
+                x1, y1, x2, y2 = map(int, box.xyxy[0])
+
+                cls = int(box.cls[0])
+                class_name = class_names[cls]
+
+                conf = float(box.conf[0])
+
+                colour = (0,255,0)
+
+                cv2.rectangle(result_frame, (x1, y1), (x2, y2), colour, 2)
+
+                cv2.putText(result_frame, f"{class_name} {conf:.2f}",
+                            (x1, max(y1 - 10, 20)), cv2.FONT_HERSHEY_SIMPLEX,
+                            0.6, colour, 2)
+    cv2.imshow("result",result_frame)
